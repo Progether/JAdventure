ad41d2e34014a1f4f27f57e6520aad75e4cb099f
hawk554 <mohawk554@gmail.com>
2013-10-30 21:43:51 -0500

    Modified TextAdventure
    This class is to be deleted and replaced by JAdventure.java which will
    hold the main method. Currently being used as a debug class.

--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class TextAdventure {
 
+	
+	/*
+	 * This class will be done away with and
+	 * JAdventure will hold the main() method
+	 * 
+	 * Using this class for now to debug methods.
+	 */
     public static void main(String[] args) {
         // Initialize the game
-        Game game = new Game();
+        // Game game = new Game();
         // game.initialize();
-	game.commands();
+    	// game.commands();
+    	Menus menu = new Menus();
+    	Scanner input = new Scanner(System.in);
+    	
+    	while (true){
+    		JSONReader json = new JSONReader();
+    		json.getMenu(input.next());
+    	}
     }
 
 }
