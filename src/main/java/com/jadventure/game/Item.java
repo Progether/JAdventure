f6300dc7b6c671eec640742c6aa1bcfc8c1bb79d
Reuben Castelino <projectdelphai@gmail.com>
2013-11-14 18:18:48 -0800

    fully transitioned to using a file for items

--- src/main/java/com/jadventure/game/Item.java
+++ src/main/java/com/jadventure/game/Item.java
@@ -1,7 +1,19 @@
 package com.jadventure.game;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 
 public class Item {
     private String name;
@@ -16,23 +28,31 @@ public class Item {
     }
 
     public HashMap<String,String> lookUpItem(int itemID) {
-        final HashMap<String,String> milk = new HashMap()
-        {{
-             put("name", "milk");
-             put("description", "some delicious milk");
-             put("type", "food");
-             put("healthIncrease", "5");
-        }};
-        HashMap<String,HashMap> items = new HashMap()
-        {{
-             put("1", milk);
-        }};
+        HashMap<String,HashMap> items = loadItems();
         for (Map.Entry<String,HashMap> entry : items.entrySet()) {
             if (entry.getKey().equals(Integer.toString(itemID))) {
                 return entry.getValue();
             }
         }
         return new HashMap();
+    }
+
+    public HashMap<String,HashMap> loadItems() {
+        String fileName = "json/items.json";
+        HashMap<String,HashMap> hashMap = new HashMap();
+        try {
+            Reader reader = new FileReader(fileName);
+            Type typeHashMap = new TypeToken<HashMap<String,HashMap<String,HashMap>>>() { }.getType();
+            Gson gson = new GsonBuilder().create();
+            HashMap<String,HashMap> outerHashMap = gson.fromJson(reader, typeHashMap);
+            hashMap = outerHashMap.get("items");
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println( "Unable to open file '" + fileName + "'.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return hashMap;
     }
 
 }
