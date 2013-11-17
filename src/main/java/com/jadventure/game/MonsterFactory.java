e7c491eb9a06ed41e56e3a675d78866dd7935abc
Reuben Castelino <projectdelphai@gmail.com>
2013-11-16 21:49:06 -0800

    style for monsterfactory

--- src/main/java/com/jadventure/game/MonsterFactory.java
+++ src/main/java/com/jadventure/game/MonsterFactory.java
@@ -8,50 +8,50 @@ import java.util.Random;
  * to the players level and location
  */
 public class MonsterFactory {
-	
-	public Monster generateMonster(Player player) {
-		Random random = new Random();
-		int randomInt;
-		switch (player.getLocationType()) {
-			case "forest":
-				randomInt = random.nextInt(3);
-				if (randomInt == 0)
-					return new Troll(player.level);
-				else if (randomInt == 1)
-					return new Goblin(player.level);
-				else
-					return new Bugbear(player.level);
-			case "swamp":
-				randomInt = random.nextInt(2);
-				return (randomInt == 1) ? new Goblin(player.level) : new Troll(player.level);
-				
-			case "mountain":
-				randomInt = random.nextInt(4);
-				if (randomInt == 0)
-					return new Giant(player.level);
-				else if (randomInt == 1)
-					return new Troll(player.level);
-				else if (randomInt == 2)
-					return new Wolf(player.level);
-				else
-					return new Skeleton(player.level);
-				
-			case "cave":
-				randomInt = random.nextInt(3);
-				if (randomInt == 0)
-					return new Troll(player.level);
-				else if (randomInt == 1)
-					return new Skeleton(player.level);
-				else
-					return new Goblin(player.level);
-				
-			case "plains":
-				randomInt = random.nextInt(2);
-				return (randomInt == 1) ? new Bugbear(player.level) : new Goblin(player.level) ;
-				
-			default: // any non-hostile location
-				return null;
-		}
-	}
-
+    
+    public Monster generateMonster(Player player) {
+        Random random = new Random();
+        int randomInt;
+        switch (player.getLocationType()) {
+            case "forest":
+                randomInt = random.nextInt(3);
+                if (randomInt == 0)
+                    return new Troll(player.level);
+                else if (randomInt == 1)
+                    return new Goblin(player.level);
+                else
+                    return new Bugbear(player.level);
+            case "swamp":
+                randomInt = random.nextInt(2);
+                return (randomInt == 1) ? new Goblin(player.level) : new Troll(player.level);
+                
+            case "mountain":
+                randomInt = random.nextInt(4);
+                if (randomInt == 0)
+                    return new Giant(player.level);
+                else if (randomInt == 1)
+                    return new Troll(player.level);
+                else if (randomInt == 2)
+                    return new Wolf(player.level);
+                else
+                    return new Skeleton(player.level);
+                
+            case "cave":
+                randomInt = random.nextInt(3);
+                if (randomInt == 0)
+                    return new Troll(player.level);
+                else if (randomInt == 1)
+                    return new Skeleton(player.level);
+                else
+                    return new Goblin(player.level);
+                
+            case "plains":
+                randomInt = random.nextInt(2);
+                return (randomInt == 1) ? new Bugbear(player.level) : new Goblin(player.level) ;
+                
+            default: // any non-hostile location
+                return null;
+        }
+    }
+    
 }
