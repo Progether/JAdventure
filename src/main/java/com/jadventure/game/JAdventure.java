66cf752b2dbdbc5240217fe9d1eec464462c7339
Peter Maatman <blackwolf12333@gmail.com>
2013-12-17 20:14:55 +0100

    Start some minimal documentation

--- src/main/java/com/jadventure/game/JAdventure.java
+++ src/main/java/com/jadventure/game/JAdventure.java
@@ -2,6 +2,11 @@ package com.jadventure.game;
 
 import com.jadventure.game.menus.MainMenu;
 
+/**
+ * This is the starting point of the game.
+ * This class doesn't do much more than create a new MainMenu
+ * that will handle the rest of the game.
+ */
 public class JAdventure {
     public static void main(String[] args) {
         new MainMenu();
