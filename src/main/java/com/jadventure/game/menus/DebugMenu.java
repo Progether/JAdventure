17c6d66b7c430e53887c8684028399a34e64b7ea
Shubham Keserwani <shubhamkesar@live.com>
2014-01-02 11:32:01 +0530

    And the 'p's.

--- src/main/java/com/jadventure/game/menus/DebugMenu.java
+++ src/main/java/com/jadventure/game/menus/DebugMenu.java
@@ -16,13 +16,13 @@ public class DebugMenu extends Menus {
     public DebugMenu(Player p) {
         this.menuID = 90;
         player = p;
-        this.menuItems.add(new MenuItem("pAttack", "Modify Player Attack"));
-        this.menuItems.add(new MenuItem("pHealth", "Modify Player Health"));
-        this.menuItems.add(new MenuItem("pMaxHealth", "Modify Player Max Health"));
-        this.menuItems.add(new MenuItem("pArmour", "Modify Player Armour"));
-        this.menuItems.add(new MenuItem("pLevel", "Modify Player Level"));
-        this.menuItems.add(new MenuItem("pGold", "Modify Player Gold"));
-        this.menuItems.add(new MenuItem("pBackpack", "Modify Player Backpack"));
+        this.menuItems.add(new MenuItem("Attack", "Modify Player Attack"));
+        this.menuItems.add(new MenuItem("Health", "Modify Player Health"));
+        this.menuItems.add(new MenuItem("MaxHealth", "Modify Player Max Health"));
+        this.menuItems.add(new MenuItem("Armour", "Modify Player Armour"));
+        this.menuItems.add(new MenuItem("Level", "Modify Player Level"));
+        this.menuItems.add(new MenuItem("Gold", "Modify Player Gold"));
+        this.menuItems.add(new MenuItem("Backpack", "Modify Player Backpack"));
         this.menuItems.add(new MenuItem("Stats", "Display current player stats"));
         this.menuItems.add(new MenuItem("Exit", "Exits Debug Menu"));
 
@@ -44,20 +44,20 @@ public class DebugMenu extends Menus {
         String key = m.getKey();
 
         try{
-            if(key.equals("pattack")){
+            if(key.equals("attack")){
                 System.out.println("Current attack : " + player.getDamage());
                 System.out.print("Enter new attack : ");
                 int newVal = input.nextInt();
                 player.setDamage(newVal);
             }
-            else if(key.equals("pmaxhealth")){
+            else if(key.equals("maxhealth")){
                 System.out.println("Current maximum health : " + player.getHealthMax());
                 System.out.print("Enter new maximum health : ");
                 int newVal = input.nextInt();
                 player.setHealthMax(newVal);
                 player.setHealth(newVal < player.getHealth() ? newVal : player.getHealth());
             }
-            else if(key.equals("phealth")){
+            else if(key.equals("health")){
                 System.out.println("Current health : " + player.getHealth());
                 System.out.print("Enter new health : ");
                 int newVal = input.nextInt();
@@ -66,25 +66,25 @@ public class DebugMenu extends Menus {
                     player.setHealthMax(newVal);
                 player.setHealth(newVal);
             }
-            else if(key.equals("parmour")){
+            else if(key.equals("armour")){
                 System.out.println("Current armour : " + player.getArmour());
                 System.out.print("New armour value : ");
                 int newVal = input.nextInt();
                 player.setArmour(newVal);
             }
-            else if(key.equals("plevel")){
+            else if(key.equals("level")){
                 System.out.println("Current level : " + player.getArmour());
                 System.out.print("New level : ");
                 int newVal = input.nextInt();
                 player.setLevel(newVal);
             }
-            else if(key.equals("pgold")){
+            else if(key.equals("gold")){
                 System.out.println("Current gold : " + player.getArmour());
                 System.out.print("New gold amount : ");
                 int newVal = input.nextInt();
                 player.setGold(newVal);
             }
-		    else if(key.equals("pbackpack")){
+		    else if(key.equals("backpack")){
                 new BackpackDebugMenu(player);
 		    }
 		    else if(key.equals("stats"))
