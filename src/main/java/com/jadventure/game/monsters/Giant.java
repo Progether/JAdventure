b429b1ca7fb5311bfb2ea2c06a3b4cc7eb856dab
Reuben Castelino <projectdelphai@gmail.com>
2014-01-04 19:05:03 -0800

    changed Giant values to reflect description

--- src/main/java/com/jadventure/game/monsters/Giant.java
+++ src/main/java/com/jadventure/game/monsters/Giant.java
@@ -6,10 +6,10 @@ package com.jadventure.game.monsters;
 public class Giant extends Monster {
 	public Giant(int playerLevel){
 		this.monsterType = "Giant";
-		this.setHealthMax(100 + playerLevel * 8);
-		this.setHealth(100 + playerLevel * 8);
+		this.setHealthMax(150 + playerLevel * 8);
+		this.setHealth(150 + playerLevel * 8);
 		this.setArmour(6 + playerLevel * 3);
-		this.setDamage(17 + playerLevel * 3);
+		this.setDamage(40 + playerLevel * 3);
 		this.setCritChance(3);
 		this.setGold(15 + playerLevel * 11);
 	}
