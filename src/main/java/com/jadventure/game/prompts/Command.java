ca89e183277e32192cd1b36b6f1c6e2f4723fc6a
Tjeerd Verhagen <Tjeerd.Verhagen@ing.nl>
2014-07-03 14:43:14 +0200

    Adds target 'method' to this annotation.

--- src/main/java/com/jadventure/game/prompts/Command.java
+++ src/main/java/com/jadventure/game/prompts/Command.java
@@ -1,13 +1,16 @@
 package com.jadventure.game.prompts;
 
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Command annotates a command method in the CommandCollection
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Command {
     String command();
     String aliases();
