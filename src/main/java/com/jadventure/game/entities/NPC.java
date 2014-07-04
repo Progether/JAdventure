f7a4723751fa045ab631186be2037f3198fa4bb4
Tjeerd Verhagen <Tjeerd.Verhagen@ing.nl>
2014-07-04 16:57:28 +0200

    #2 Improve JavaDoc

--- src/main/java/com/jadventure/game/entities/NPC.java
+++ src/main/java/com/jadventure/game/entities/NPC.java
@@ -1,10 +1,10 @@
 package com.jadventure.game.entities;
 
 /**
- * This class deals with Non Player Character (npc) and all of their properties.
- * Any method that changes a npc or a player  interacts with it should
+ * This class deals with Non Player Character (NPC) and all of their properties.
+ * Any method that changes a NPC or a player  interacts with it should
  * be placed within this class. If a method deals with entities in general or
- * with variables not unique to the npc, place it in the entity class.
+ * with variables not unique to the NPC, place it in the entity class.
  */
 public class NPC extends Entity {
     
