78636e2e850f7056d20a9b355b2d91d848807196
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2014-07-13 21:02:44 +0200

    #9 Adds method void execute(Player player, IGameElementVisitor visitor, String[] args)

--- src/main/java/com/jadventure/game/command/ICommand.java
+++ src/main/java/com/jadventure/game/command/ICommand.java
@@ -2,11 +2,17 @@ package com.jadventure.game.command;
 
 import java.util.List;
 
+import com.jadventure.game.IGameElementVisitor;
+import com.jadventure.game.entities.Player;
+
 public interface ICommand {
 
     String getCommandName();
+    
     List<String> getCommandAliases();
     
     String getDescription();
     
+    void execute(Player player, IGameElementVisitor visitor, String[] args);
+
 }
