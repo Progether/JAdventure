e60230225998b591ebdb850acdbea2e169cae3af
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2014-07-14 20:57:02 +0200

    #9 Replaces implementation, which is now using the ICommand instances (was CommandCollection)

--- src/main/java/com/jadventure/game/prompts/CommandParser.java
+++ src/main/java/com/jadventure/game/prompts/CommandParser.java
@@ -1,109 +1,58 @@
 package com.jadventure.game.prompts;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.TreeMap;
 
+import com.jadventure.game.IGameElementVisitor;
+import com.jadventure.game.command.BackpackCommand;
+import com.jadventure.game.command.DequipCommand;
+import com.jadventure.game.command.DropCommand;
+import com.jadventure.game.command.EquipCommand;
+import com.jadventure.game.command.GotoCommand;
+import com.jadventure.game.command.HelpCommand;
+import com.jadventure.game.command.ICommand;
+import com.jadventure.game.command.InspectCommand;
+import com.jadventure.game.command.LookCommand;
+import com.jadventure.game.command.MonsterCommand;
+import com.jadventure.game.command.PickCommand;
+import com.jadventure.game.command.SaveCommand;
+import com.jadventure.game.command.StatisticsCommand;
 import com.jadventure.game.entities.Player;
 
 /**
- * CommandParser parses the game commands
- *
- * It parses all the commands automatically.
- * To add a new command, you just need to make addition in the CommandCollection.
  */
 public class CommandParser {
-    /** Command and alias names to  */
-    private TreeMap<String, Method> commandMap = new TreeMap<String, Method>();
+	private TreeMap<String, ICommand> commands = new TreeMap<>();
+	
+	public CommandParser() {
+		// TODO Improve auto adding of ICommand instance, by reflection, search on ICommand
+		add(new BackpackCommand());
+		add(new DequipCommand());
+		add(new DropCommand());
+		add(new EquipCommand());
+		add(new GotoCommand());
+		add(new HelpCommand());
+		add(new InspectCommand());
+		add(new LookCommand());
+		add(new MonsterCommand());
+		add(new PickCommand());
+		add(new SaveCommand());
+		add(new StatisticsCommand());
+	}
 
-    public CommandParser() {
-        initCommandMap();
-    }
+	private void add(ICommand command) {
+		commands.put(command.getCommandName(), command);
+		for (String alias : command.getCommandAliases()) {
+			commands.put(alias, command);
+		}
+	}
 
-    // adds the command to the commandMap
-    private void initCommandMap() {
-        Method[] methods = CommandCollection.class.getMethods();
 
-        for (Method method: methods) {
-            if (! method.isAnnotationPresent(Command.class)) {
-                continue;
-            }
-
-            Command annotation = method.getAnnotation(Command.class);
-            this.commandMap.put(annotation.command(), method);
-            for (String alias : annotation.aliases().split(",")) {
-                System.out.println("Command alias: '" + alias + "'");
-                if (alias.length() == 0) {
-                    break;
-                }
-                this.commandMap.put(alias, method);
-            }
-        }
-    }
-
-    public boolean parse(Player player, String command, String[] args) {
-        CommandCollection com = CommandCollection.getInstance();
-        com.initPlayer(player);
-        System.out.println("Command '" + command + "' " + toString(args));
-
-        if(command.equalsIgnoreCase("exit")) {
-            return false;
-        }
-
-        // descendingKeySet otherwise startsWith will return correspond to longer command
-        // e.g. 'de' will match startWith('d')
-        for (String key : commandMap.descendingKeySet()) {
-            if (command.startsWith(key)) {
-                Method method = commandMap.get(key);
-                if (method.getParameterTypes().length == 0) {
-                    try {
-                        method.invoke(com);
-                    }
-                    catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else if (method.getParameterTypes()[0] == String.class) {
-                    String arg = args[0];
-                    System.out.println("Argument '" + arg + "'");
-                    try {
-                        method.invoke(com, arg);
-                    }
-                    catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return true;
+	public void parse(Player player, String commandStr, String[] args, IGameElementVisitor visitor) {
+        for (String key : commands.descendingKeySet()) {
+            if (commandStr.startsWith(key)) {
+            	commands.get(key).execute(player, visitor, args);
             }
         }
-
-        return true;
-    }
-
-    public static String toString(String[] args) {
-        boolean isFirstItem = true;
-        StringBuilder bldr = new StringBuilder("[");
-        for (String arg : args) {
-            if (isFirstItem) {
-                bldr.append(" ");
-            }
-            else {
-                bldr.append(", ");
-            }
-            bldr.append(arg);
-            isFirstItem = false;
-        }
-        if (! isFirstItem) {
-            bldr.append(" ");
-        }
-        bldr.append("]");
-        return bldr.toString();
     }
 
 }
