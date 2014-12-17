91448f1d71beb2ab0a55487920b56e6e8499d899
Reuben Castelino <projectdelphai@gmail.com>
2014-12-17 15:41:13 -0800

    added more npcs, placed guide BACK onto map (along with brotherhood member), and fixed incorrect name of npc

--- src/main/java/com/jadventure/game/entities/NPC.java
+++ src/main/java/com/jadventure/game/entities/NPC.java
@@ -1,5 +1,25 @@
 package com.jadventure.game.entities;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.reflect.TypeToken;
+
+import com.jadventure.game.QueueProvider;
+
 /**
  * This class deals with Non Player Character (NPC) and all of their properties.
  * Any method that changes a NPC or a player  interacts with it should
@@ -9,7 +29,22 @@ package com.jadventure.game.entities;
 public class NPC extends Entity {
     
     public NPC(String entityID) {
-        setName(entityID);
+        JsonParser parser = new JsonParser();
+        String fileName = "json/npcs.json";
+
+        try {
+            Reader reader = new FileReader(fileName);
+            JsonObject npcs = parser.parse(reader).getAsJsonObject().get("npcs").getAsJsonObject();
+            JsonObject json = new JsonObject();
+            for (Map.Entry<String, JsonElement> entry : npcs.entrySet()) {
+                if (entry.getKey().equals(entityID)) {
+                    json = entry.getValue().getAsJsonObject();
+                }
+            }
+            setName(json.get("name").getAsString());
+        } catch (FileNotFoundException ex) {
+            QueueProvider.offer("Unable to open file '" + fileName + "'.");
+        }
     }
 
 }
