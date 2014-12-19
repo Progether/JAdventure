4ad6d9166c12e96becc2c5f6ee3cfac8298e49ef
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2014-12-19 21:05:04 +0100

    Removes unused imports

--- src/main/java/com/jadventure/game/entities/NPC.java
+++ src/main/java/com/jadventure/game/entities/NPC.java
@@ -1,23 +1,13 @@
 package com.jadventure.game.entities;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Reader;
-import java.io.Writer;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.reflect.TypeToken;
-
 import com.jadventure.game.QueueProvider;
 
 /**
