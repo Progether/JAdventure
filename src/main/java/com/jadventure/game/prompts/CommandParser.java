ff006e565aa4e759a673ec029140520b6be263e8
Reuben Castelino <projectdelphai@gmail.com>
2014-12-30 21:16:36 -0800

    edited parsing so that commands with AND without spaces are valid

--- src/main/java/com/jadventure/game/prompts/CommandParser.java
+++ src/main/java/com/jadventure/game/prompts/CommandParser.java
@@ -75,7 +75,10 @@ public class CommandParser {
                         }
                     }
                 } else if (method.getParameterTypes()[0] == String.class) {
-                    String arg = command.substring(key.length() + 1);
+                    String arg = command.substring(key.length());
+                    if (arg.charAt(0) == ' ') {
+                        arg = command.substring(key.length() + 1);
+                    }
                     try {
                         if (method.getAnnotation(Command.class).debug()) {
                             if ("test".equals(player.getName())) {
