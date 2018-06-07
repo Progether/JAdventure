3e3e0eccf873346a74e6ae2003709beb34eead4a
Kyle Robbertze <krobbertze@gmail.com>
2018-06-07 20:51:30 +0200

    Add Monster factory unit test

--- src/main/java/com/jadventure/game/monsters/Giant.java
+++ src/main/java/com/jadventure/game/monsters/Giant.java
@@ -4,18 +4,18 @@ package com.jadventure.game.monsters;
  * A monster with high health and damage, but low armour.
  */
 public class Giant extends Monster {
-	public Giant(int playerLevel){
-		this.monsterType = "Giant";
-		this.setHealthMax(150 + playerLevel * 8);
-		this.setHealth(150 + playerLevel * 8);
-		this.setArmour(6 + playerLevel * 3);
-		this.setDamage(40 + playerLevel * 3);
-                this.setIntelligence(3);
-                this.setStealth(1);
-                this.setDexterity(1);
-		this.setCritChance(0.03);
-                this.setXPGain(50 + playerLevel * 3);
-		this.setGold(15 + playerLevel * 11);
-                addRandomItems(playerLevel, "wrbd1");
-	}
+    public Giant(int playerLevel){
+        this.monsterType = "Giant";
+        this.setHealthMax(150 + playerLevel * 8);
+        this.setHealth(150 + playerLevel * 8);
+        this.setArmour(6 + playerLevel * 3);
+        this.setDamage(40 + playerLevel * 3);
+        this.setIntelligence(3);
+        this.setStealth(1);
+        this.setDexterity(1);
+        this.setCritChance(0.03);
+        this.setXPGain(50 + playerLevel * 3);
+        this.setGold(15 + playerLevel * 11);
+        addRandomItems(playerLevel, "wbrd1");
+    }
 }
---
+++ src/test/java/com/jadventure/game/monsters/MonsterFactoryTest.java
@@ -0,0 +1,92 @@
+package com.jadventure.game.monsters;
+
+import static org.junit.Assert.*;
+
+import java.security.PublicKey;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jadventure.game.entities.Player;
+import com.jadventure.game.navigation.Coordinate;
+import com.jadventure.game.navigation.ILocation;
+import com.jadventure.game.navigation.Location;
+import com.jadventure.game.navigation.LocationType;
+
+public class MonsterFactoryTest {
+    private Player player;
+    private Location location;
+    private MonsterFactory factory;
+
+    @Before
+    public void setUp(){
+        player = Player.getInstance("recruit");
+
+        Coordinate coordinate = new Coordinate(1, 1, 0);
+        String title = "At the edge of a forest";
+        String description = "The are many big trees and some tick busses, " +
+            "looks difficult to go through.";
+        LocationType locationType = LocationType.FOREST;
+        location = new Location(coordinate, title, description, locationType);
+        location.setDangerRating(5);
+
+        factory = new MonsterFactory();
+    }
+
+    @Test
+    public void localizedMonsterGenerationTest_Forest() {
+        location.setLocationType(LocationType.FOREST);
+        player.setLocation(location);
+        Monster forestMonster = factory.generateMonster(player);
+        assertTrue(forestMonster instanceof Troll ||
+                forestMonster instanceof Bugbear ||
+                forestMonster instanceof Goblin);
+    }
+
+    @Test
+    public void localizedMonsterGenerationTest_Swamp() {
+        location.setLocationType(LocationType.SWAMP);
+        player.setLocation(location);
+        Monster swampMonster = factory.generateMonster(player);
+        assertTrue(swampMonster instanceof Goblin ||
+                swampMonster instanceof Troll);
+    }
+
+    @Test
+    public void localizedMonsterGenerationTest_Mountains() {
+        location.setLocationType(LocationType.MOUNTAIN);
+        player.setLocation(location);
+        Monster mountainMonster = factory.generateMonster(player);
+        assertTrue(mountainMonster instanceof Giant ||
+                mountainMonster instanceof Wolf ||
+                mountainMonster instanceof Troll ||
+                mountainMonster instanceof Skeleton);
+    }
+
+    @Test
+    public void localizedMonsterGenerationTest_Cave() {
+        location.setLocationType(LocationType.CAVE);
+        player.setLocation(location);
+        Monster caveMonster = factory.generateMonster(player);
+        assertTrue(caveMonster instanceof Troll ||
+                caveMonster instanceof Skeleton ||
+                caveMonster instanceof Goblin);
+    }
+
+    @Test
+    public void localizedMonsterGenerationTest_Plains() {
+        location.setLocationType(LocationType.PLAINS);
+        player.setLocation(location);
+        Monster plansMonster = factory.generateMonster(player);
+        assertTrue(plansMonster instanceof Bugbear ||
+                plansMonster instanceof Goblin);
+    }
+
+    @Test
+    public void monsterGenerationOnNoDangerLocation(){
+        location.setDangerRating(0);
+        player.setLocation(location);
+        Monster monster = factory.generateMonster(player);
+        assertNull(monster);
+    }
+}
