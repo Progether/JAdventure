dd19f62a8464097522682569ff60f231d532ea0d
Christos-Michail Faltsetas <chrisfaltsetas@gmail.com>
2019-05-03 19:06:30 +0300

    Initialize controller references for Main Menu

--- src/main/java/com/jadventure/game/MainMenuController.java
+++ src/main/java/com/jadventure/game/MainMenuController.java
@@ -1,13 +1,60 @@
 package com.jadventure.game;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.stage.WindowEvent;
 
 public class MainMenuController {
+    
     // Reference to the main application.
     private JAdventure jAdventure;
 
     public MainMenuController() {
+    }
+    
+    @FXML
+    private Button start;
+    @FXML
+    private Button load;
+    @FXML
+    private Button quit;
+    @FXML
+    private MenuItem save;
+    @FXML
+    private MenuItem back;
+    @FXML
+    private MenuItem close;
+    @FXML
+    private MenuItem github;
+    
+    @FXML
+    public void startGame() {
+        
+    }
+    
+    @FXML
+    public void loadProfile() {
+        
+    }
+    
+    @FXML
+    public void saveGame() {
+        
+    }
+    @FXML
+    public void goToMainMenu() {
+        
+    }
+    @FXML
+    public void goToGithub() {
+        
+    }
+    
+    @FXML
+    public void quitGame() throws Exception {
+        jAdventure.getPrimaryStage().fireEvent(
+                new WindowEvent(jAdventure.getPrimaryStage(), WindowEvent.WINDOW_CLOSE_REQUEST));
     }
     
     /**
